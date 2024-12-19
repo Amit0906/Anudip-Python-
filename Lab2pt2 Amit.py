bcbd42273660522c61b720a8e0f4832ef3a46072
Amit0906 <amitsmmandal@gmail.com>
2024-12-19 16:55:05 +0530

    Python programming Lab 2
    Task 1: Write a program to input 3 sides of a triangle and calculate and print its area.
    Task2: Write a program to input the temperature in Celsius and calculate and print it in Farhenheit.

---
+++ Lab2pt2 Amit.py
@@ -0,0 +1,9 @@
+#Write a program to input the temperature in celcius
+#and calculate ans print it in Farhenheit.
+
+#take celcius degree from user
+celcius=int(input("Enter the Celcius temperature: "))
+
+#convert it to fahrenheit
+fahrenheit=(celcius* 9/5) + 32
+print("The temperature in Fahrenheit is ",fahrenheit)
